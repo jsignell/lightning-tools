4d70247dcd0aa3fd8d51326acfc128a985706a3d
Julia Signell <jsignell@gmail.com>
2016-06-07 21:54:37 +0000

    restructured to create point-process

--- lightning/r-tools.R
+++
@@ -1,7 +0,0 @@
-library('SpatialVx')
-library('stringr')
-
-FeatureFinder_gaussian <- function(hold, nx=199, ny=199, ...) {
-    look <- FeatureFinder(hold, smoothfun="gauss2dsmooth", smoothfunargs=c(nx=nx, ny=ny), ...)
-    return(look)
-}
