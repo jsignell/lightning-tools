51a0925316fcad75ebf759bb679da0d0d0d1268a
Julia Signell <jsignell@gmail.com>
2016-07-02 11:14:05 +0000

    interesting new notebooks

--- titan.py
+++ titan.py
@@ -19,27 +19,32 @@ def read_TITAN(fname):
         df = pd.read_csv(fname, skiprows=9, delim_whitespace=True, header=None, names=header)
     return(df)
 
-def storm_locations(df, ax=None, paths=False, zoom=7, zorder=5):
-    from cartopy.io.img_tiles import StamenTerrain
-    storm_names = dict([(n[1], n[0]) for n in enumerate(df.ComplexNum.unique())])
-    gb = df.groupby(df.ComplexNum)
+def feature_locations(df, ax=None, lat='ReflCentroidLat(deg)', lon='ReflCentroidLon(deg)',
+                    paths=False, features=True, zoom=7, zorder=5, colorby='ComplexNum', c='k'):
     if ax is None:
+        from cartopy.io.img_tiles import StamenTerrain
         plt.figure(figsize=(14, 8))
         ax = plt.axes(projection=ccrs.PlateCarree())
         background(ax)
         ax.add_image(StamenTerrain(), zoom)
-    df.plot.scatter(x='ReflCentroidLon(deg)', 
-                    y='ReflCentroidLat(deg)', 
-                    c=[storm_names[n] for n in df['ComplexNum']],
-                    ax=ax, cmap='rainbow',
-                    edgecolor='None', s=50, zorder=zorder)
+    if features:
+        storm_names = dict([(n[1], n[0]) for n in enumerate(df[colorby].unique())])
+        df.plot.scatter(x=lon, y=lat, 
+                        c=[storm_names[n] for n in df[colorby]],
+                        ax=ax, cmap='rainbow',
+                        edgecolor='None', s=50, zorder=zorder)
     if paths:
+        gb = df.groupby(df['ComplexNum'])
         for k,v in gb.groups.items():
-            gb.get_group(k).plot(x='ReflCentroidLon(deg)', c='k',
-                                 y='ReflCentroidLat(deg)', ax=ax, legend=None, zorder=6)
+            gb.get_group(k).plot(x=lon, y=lat, c=c, ax=ax, legend=None, zorder=zorder+1)
     return(ax)
 
 def bearing_plot(df, ax=None, N=16, bottom=0):
+    '''
+    This one is a little tricky because at initiation the direction is set to zero, 
+    so we first need to get rid of all the zeros. Luckily we know that there are no 
+    actual zero values because 360 is included in the possible directions
+    '''
     if ax is None:
         ax = plt.subplot(111, polar=True)
     theta = np.linspace(0.0, 2 *np.pi, N, endpoint=False)
@@ -63,7 +68,7 @@ def windrose(df, dirn='Dirn(DegT)', speed='Speed(km/hr)', ax=None, N=16, bottom=
     ax.set_theta_zero_location("S")
     ax.set_theta_direction(-1)
     width = (2*np.pi) / N
-    srange = zip([0,5,10,20,50], [5,10,20,50,100], ['blue','green','yellow','#FF7800','red']) 
+    srange = zip([0,5,10,20,50], [5,10,20,50,100], ['#0000dd','green','#dddd00','#FF7800','#dd0000']) 
     ntot = df[dirn][df[dirn]>0].count()
     
     radii0 = [bottom]*N
@@ -74,27 +79,3 @@ def windrose(df, dirn='Dirn(DegT)', speed='Speed(km/hr)', ax=None, N=16, bottom=
         bars = ax.bar(theta, radii, width=width, bottom=radii0, facecolor=c, alpha=0.8)
         #print smin, smax, c, radii
         radii0+= radii
-
-def windrose_cbar(fig=None):
-    '''
-    If you have a figsize in mind, then pass a figure object to this function
-    and the colorbar will be drawn to suit
-    '''
-    if fig is None:
-        fig = plt.figure(figsize=(16,1))
-    y = fig.get_figwidth()
-    import matplotlib.patches as mpatch
-    srange = zip([0,5,10,20,50], [5,10,20,50,100], ['#0000dd','green','#dddd00','#FF7800','#dd0000']) 
-    n=1
-    for smin, smax, c in srange:
-        ax = plt.subplot(1,5,n)
-        patch = mpatch.FancyBboxPatch([0,0], 1, 1, boxstyle='square', facecolor=c)
-        ax.add_patch(patch)
-        plt.axis('off')
-        if y>=12:
-            ax.text(.1, .4, '{smin} - {smax} km/hr'.format(smin=smin, smax=smax),
-                    fontsize=min(18, y+2), fontdict = {'color': 'white'})
-        else:
-            ax.text(.1, .4, '{smin} - {smax}\nkm/hr'.format(smin=smin, smax=smax),
-                    fontsize=min(14, y+5), fontdict = {'color': 'white'})
-        n+=1
