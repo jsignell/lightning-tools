5b7a39fd6761d0c8b5d0a12a1faf640b561acbd3
Julia Signell <jsignell@gmail.com>
2016-07-06 21:13:47 +0000

    added tilt exploration tools

--- titan.py
+++ titan.py
@@ -47,11 +47,11 @@ def bearing_plot(df, ax=None, N=16, bottom=0):
     '''
     if ax is None:
         ax = plt.subplot(111, polar=True)
-    theta = np.linspace(0.0, 2 *np.pi, N, endpoint=False)
-    radii, _ = np.histogram(df['Dirn(DegT)'][df['Dirn(DegT)']>0].values, bins=N)
+    theta = np.linspace(0.0, 2 *np.pi, N+1)
+    radii, _ = np.histogram(df['Dirn(DegT)'][df['Dirn(DegT)']>0].values, bins=(theta/np.pi*180))
     width = (2*np.pi) / N
 
-    bars = ax.bar(theta, radii, width=width, bottom=bottom)
+    bars = ax.bar(theta[:-1], radii, width=width, bottom=bottom)
     ax.set_theta_zero_location("S")
     ax.set_theta_direction(-1)
 
@@ -64,7 +64,7 @@ def bearing_plot(df, ax=None, N=16, bottom=0):
 def windrose(df, dirn='Dirn(DegT)', speed='Speed(km/hr)', ax=None, N=16, bottom=0):
     if ax is None:
         ax = plt.subplot(111, polar=True)
-    theta = np.linspace(0.0, 2 *np.pi, N, endpoint=False)
+    theta = np.linspace(0.0, 2 *np.pi, N+1)
     ax.set_theta_zero_location("S")
     ax.set_theta_direction(-1)
     width = (2*np.pi) / N
@@ -74,8 +74,34 @@ def windrose(df, dirn='Dirn(DegT)', speed='Speed(km/hr)', ax=None, N=16, bottom=
     radii0 = [bottom]*N
     for smin, smax, c in srange:
         cond = ((df[dirn]>0) & (df[speed]>=smin) & (df[speed]<smax))
-        radii, _ = np.histogram(df[dirn][cond].values, bins=N)
+        radii, _ = np.histogram(df[dirn][cond].values, bins=(theta/np.pi*180))
         radii = radii/float(ntot)*100
-        bars = ax.bar(theta, radii, width=width, bottom=radii0, facecolor=c, alpha=0.8)
+        bars = ax.bar(theta[:-1], radii, width=width, bottom=radii0, facecolor=c, alpha=0.8)
         #print smin, smax, c, radii
         radii0+= radii
+
+def up_tilt(s, yp, xp):
+    x0 = s.lon
+    y0 = s.lat
+    if (0 < s.tilt_orient <=45) or (315<s.tilt_orient <=360) and yp>y0:
+        return True
+    elif (45 < s.tilt_orient <=135) and xp>x0:
+        return True
+    elif (135 < s.tilt_orient <=225) and yp<y0:
+        return True
+    elif (225 < s.tilt_orient <=315) and xp<x0:
+        return True
+    else:
+        return False
+
+def in_envelope(s, yp, xp, by=1):
+    from math import cos, sin, radians,  pi
+    alpha = radians(-90-s.orient)
+    x0 = s.lon
+    y0 = s.lat
+    a = s.major/100.
+    b = s.minor/100.
+    if (((cos(alpha)*(xp-x0)+sin(alpha)*(yp-y0))**2)/(a**2) + ((sin(alpha)*(xp-x0)-cos(alpha)*(yp-y0))**2)/(b**2)) <=by:
+        return True
+    else:
+        return False
